fb26fa1d9cc7bd6d5468d1649066e4d876a4d83e
CodeRacon <michael.buschmann.86@gmail.com>
2023-11-12 22:31:10 +0100

    further on doing epic shit

---
+++ script.js
@@ -0,0 +1,16 @@
+// typewriter-effect
+
+quizData = [
+  {
+    question: 'The Beatles on a usual tuesday. Which song do we "see" here?',
+    answers: [
+      'Now and Then',
+      'Norwegian Wood',
+      'Strawberry Fields',
+      'Lovely Rita',
+    ],
+    rightOne: '2',
+  },
+];
+
+const cardText = quizData[0].question;
